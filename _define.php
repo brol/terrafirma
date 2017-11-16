48e370e223c4d2c8803679b16de03e7c5f7cd7ca
brol <brol31@gmail.com>
2017-11-16 19:01:53 +0100

    1.3 cf changelog

---
+++ _define.php
@@ -0,0 +1,22 @@
+<?php
+# ***** BEGIN LICENSE BLOCK *****
+#
+#  	Terra Firma
+#   Original Theme by NodeThirtyThree (URI : http://www.nodethirtythree.com)
+#  	Theme by David Yim
+#   Contributor : Pierre Van Glabeke
+#   License : public domain
+#
+# ***** END LICENSE BLOCK *****
+
+$this->registerModule(
+	/* Name */			    "Terra Firma",
+	/* Description*/		"Original Theme by NodeThirtyThree",
+	/* Author */			  "David Yim (http://davidyim.fr/), Pierre Van Glabeke",
+	/* Version */			  '1.3',
+	array(
+		'type'	 =>	'theme',
+		'tplset' => 'mustek',
+		'dc_min' => '2.9'
+	)
+);
---
+++ _public.php
@@ -0,0 +1,13 @@
+<?php
+# ***** BEGIN LICENSE BLOCK *****
+#
+#  	Terra Firma
+#   Original Theme by NodeThirtyThree (URI : http://www.nodethirtythree.com)
+#  	Theme by David Yim
+#   Contributor : Pierre Van Glabeke
+#   License : public domain
+#
+# ***** END LICENSE BLOCK *****
+if (!defined('DC_RC_PATH')) { return; }
+
+l10n::set(dirname(__FILE__).'/locales/'.$_lang.'/public');
